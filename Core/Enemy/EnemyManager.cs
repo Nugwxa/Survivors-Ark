b463025fb530dc645153d373202ba7108f0ceb8c
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2023-01-06 00:57:04 +0000

    Update EnemyManager.cs

--- Core/Enemy/EnemyManager.cs
+++ Core/Enemy/EnemyManager.cs
@@ -1,1 +1,28 @@
+using UnityEngine;
 
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;         // "PlayerHealth" script;
+    public GameObject enemy;                  // Enemy to be spawned (Prefab);
+    public float spawnTime = 0.5f;
+    public Transform[] spawnPoints;
+
+
+	void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	
+	void Spawn ()
+    {
+		if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
