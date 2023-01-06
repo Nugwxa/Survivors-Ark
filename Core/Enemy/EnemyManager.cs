1e32e92249fff3a94bca144ba258db8acd64f401
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2023-01-06 01:00:24 +0000

    Comments

--- Core/Enemy/EnemyManager.cs
+++ Core/Enemy/EnemyManager.cs
@@ -4,24 +4,24 @@ public class EnemyManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;         // "PlayerHealth" script;
     public GameObject enemy;                  // Enemy to be spawned (Prefab);
-    public float spawnTime = 0.5f;
-    public Transform[] spawnPoints;
+    public float spawnTime = 0.5f;            // Time between each spawn;
+    public Transform[] spawnPoints;           // Spawnpoints;
 
 
 	void Start ()
     {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        InvokeRepeating("Spawn", spawnTime, spawnTime);                 // Spawn enemies;
 	}
 	
 	
 	void Spawn ()
     {
-		if(playerHealth.currentHealth <= 0f)
+		if(playerHealth.currentHealth <= 0f)                    // If player is dead don't spawn;
         {
             return;
         }
 
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);      // Spawn Randomly
 
         Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 	}
