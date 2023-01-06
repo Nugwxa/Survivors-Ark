6e712d5f1986387da825194b65ae3d188a5c1373
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2023-01-06 00:53:18 +0000

    Comments

--- Core/Enemy/EnemyMovement.cs
+++ Core/Enemy/EnemyMovement.cs
@@ -4,30 +4,30 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour {
 
-    Transform player;                
-    PlayerHealth playerHealth;
-    EnemyHealth enemyHealth;
+    Transform player;                    // Player's Location;          
+    PlayerHealth playerHealth;           // "PlayerHealth" script;
+    EnemyHealth enemyHealth;             // "EnemyHealth" script;
     NavMeshAgent nav;
 
 
 	void Awake ()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        playerHealth = player.GetComponent <PlayerHealth> ();
-        enemyHealth = GetComponent <EnemyHealth> ();
+        player = GameObject.FindGameObjectWithTag("Player").transform;       // Get the player's locaation;
+        playerHealth = player.GetComponent <PlayerHealth> ();                // Get the player's health;
+        enemyHealth = GetComponent <EnemyHealth> ();                         // Get the enemy's health;
         nav = GetComponent <NavMeshAgent> ();
 	}
 	
 
 	void Update ()
     {
-        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)  // If the enemy & player are alive;
         {
-        nav.SetDestination (player.position);
+        nav.SetDestination (player.position);                                // Chase player;
         }
         else
         {
-         nav.enabled = false;
+         nav.enabled = false;                                                // Disable NavMesh
         }
     }
 }
