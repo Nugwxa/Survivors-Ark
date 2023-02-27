d9807f1286c5cff6c75dd9aed0bb5bd7dd4f2beb
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2023-02-27 16:59:26 +0000

    Adding comments

--- GunControl/Gun.cs
+++ GunControl/Gun.cs
@@ -1,50 +1,53 @@
-using UnityEngine;
+// Damage, range, and fire rate for the gun
+public float damage = 10f;
+public float range = 100f;
+public float fireRate = 15f;
 
-public class Gun : MonoBehaviour {
+// Damage per shot and timer for the gun
+public int damagePerShot = 10;
+float timer;
 
-    public float damage = 10f;
-    public float range = 100f;
-    public float fireRate = 15f;
-    public int damagePerShot = 10;
+// Layer mask for objects that can be shot
+int shootableMask;
 
-    float timer;
-    int shootableMask;
+// Camera for the first-person view and camera shake effect
+public Camera fpsCam;
+public CameraShake cameraShake;
 
-    public Camera fpsCam;
-    public CameraShake cameraShake;
+void Awake()
+{
+    // Set up shootable layer mask
+    shootableMask = LayerMask.GetMask("Shootable");        
+}
 
-    void Awake()
+private float nextTimeToFire = 0f;
+
+// Update is called once per frame
+void Update () {
+
+    // If the fire button is pressed and enough time has passed since the last shot, shoot and shake the camera
+    if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
     {
-        shootableMask = LayerMask.GetMask("Shootable");        
+        nextTimeToFire = Time.time + 1f / fireRate;
+        Shoot();
+        StartCoroutine(cameraShake.Shake(.15f, 1.1f));
     }
+	
+}
 
-    private float nextTimeToFire = 0f;
-
-        // Update is called once per frame
-        void Update () {
-
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
-        {
-            nextTimeToFire = Time.time + 1f / fireRate;
-            Shoot();
-            StartCoroutine(cameraShake.Shake(.15f, 1.1f));
-
-        }
-		
-	}
-
-    void Shoot()
+// Shoot a raycast from the camera's position in the direction it's facing
+void Shoot()
+{
+    RaycastHit hit;
+   if( Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, shootableMask))
     {
-        RaycastHit hit;
-       if( Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, shootableMask))
+        // If the raycast hits an object with an EnemyHealth component, damage it
+        EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
         {
-            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-            if (enemyHealth != null)
-            {
-                enemyHealth.TakeDamage (damagePerShot, hit.point);
-                
-            }
-            Debug.Log(hit.transform.name);
+            enemyHealth.TakeDamage (damagePerShot, hit.point);
         }
+        // Log the name of the object that was hit
+        Debug.Log(hit.transform.name);
     }
 }
