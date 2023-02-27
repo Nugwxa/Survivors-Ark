5496c82f7c83433738e581b12033848aa8e29b36
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2023-02-27 19:24:42 +0000

    Comments

--- UI/canvasloader.cs
+++ UI/canvasloader.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class canvasloader : MonoBehaviour
 {
-
+    // Declare a public GameObject variable called CanvasAnim
+    // This will hold a reference to the canvas animation object
     public GameObject CanvasAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-        CanvasAnim.SetActive(true);
+    
+
+        CanvasAnim.SetActive(true); // Activate the CanvasAnim GameObject to make it visible
         
     }
 
